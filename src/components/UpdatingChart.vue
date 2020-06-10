6fb4d4d11459be9cafe5132935935d0ae6ebb129
Byron Hall <hall@controlpoint.com>
2020-06-10 18:57:09 -0400

    added filtered list

--- src/components/UpdatingChart.vue
+++ src/components/UpdatingChart.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="small">
+    <div>
+      <input type="checkbox" v-model="selectAll" @click="toggleAll($event)">
+      <label>All</label>
+      <br>
+      <input type="checkbox" value="Navy" :disabled="selectAll" v-model="checkedNames" @click="toggleSingle($event)">
+      <label>Navy</label>
+      <br>
+      <input type="checkbox" value="Army" :disabled="selectAll" v-model="checkedNames" @click="toggleSingle($event)">
+      <label>Army</label>
+      <br>
+      <input type="checkbox" value="Air Forces" :disabled="selectAll" v-model="checkedNames" @click="toggleSingle($event)">
+      <label>Air Forces</label>
+      <br>
+      <input type="checkbox" value="Marine Corps" :disabled="selectAll" v-model="checkedNames" @click="toggleSingle($event)">
+      <label>Marine Corps</label>
+      <br>
+      <span>Checked Name: {{checkedNames}}</span>
+    </div>
     <pie-chart :chart-data="datacollection"></pie-chart>
-    <button @click="fillData()">Randomize</button>
+    <button @click="fillData(checkedNames)">Randomize</button>
   </div>
 </template>
 
@@ -26,27 +44,59 @@
     },
     data () {
       return {
-        datacollection: {}
+        datacollection: {},
+        selectAll: true,
+        checkedNames: ["Navy", "Army", "Air Forces", "Marine Corps"],
       }
     },
     mounted () {
-      this.fillData()
+      this.fillData(this.checkedNames)
     },
     methods: {
-      fillData () {
+      fillData (checkedNames) {
         this.datacollection = {
-          labels: ["Navy", "Marine Corps", "Army", "Air Force"],
+          labels: checkedNames,
           datasets: [
             {
               label: 'Data One',
-              backgroundColor: ['#f87979', "blue", "black", "green"],
-              data: [this.getRandomInt(), this.getRandomInt(),this.getRandomInt(), this.getRandomInt()]
+              backgroundColor: (()=>{
+                const t0 = ['#f87979', "blue", "black", "green"];
+                const a0 = [];
+                for(let i = 0; i < checkedNames.length; i+=1){
+                  a0.push(t0[i]);
+                }
+                return a0;
+              })(),
+              data: (()=>{
+                const a0 = [];
+                for(let i = 0; i < checkedNames.length; i+=1){
+                  a0.push(this.getRandomInt())
+                }
+                return a0;
+              })()
             },
           ]
         }
       },
       getRandomInt () {
         return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      },
+      toggleAll(event){
+        if(event.target.checked){
+          this.checkedNames = ["Navy", "Army", "Air Forces", "Marine Corps"];
+        } else{
+          this.checkedNames = [];
+        }
+        this.fillData(this.checkedNames);
+      },
+      toggleSingle(event){
+        if(event.target.checked){
+          this.checkedNames.push(event.target.value);
+        } else{
+          const index = this.checkedNames.indexOf(event.target.value);
+          this.checkedNames.splice(index, 1);
+        }
+        this.fillData(this.checkedNames);
       }
     }
   }
@@ -54,7 +104,6 @@
 
 <style>
   .small {
-    max-width: 600px;
-    margin:  150px auto;
+    max-width: 400px;
   }
 </style>
